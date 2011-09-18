6f82ee1d7c527f358ffa8e37965472df47879f3c
TomV <tom@viner.tv>
2011-09-18 18:15:05 +0100

    take SSIDs-to-ignore from command line so users don't have to edit source

--- update.py
+++ update.py
@@ -5,7 +5,6 @@ import subprocess
 """
 Update packages unless tethered via bandwidth capped personal mobile hotspot
 """
-BAD_SSIDS = ('Tom',)
 
 def do(cmd, debug=False):
     if debug:
@@ -14,24 +13,27 @@ def do(cmd, debug=False):
     status_code = os.system(cmd)
     assert status_code==0, 'Status code: %s' % status_code
 
-def on_mifi(bad_ssids=BAD_SSIDS):
+def on_mifi(bad_ssids=()):
     conn_data = subprocess.Popen('iwconfig', stdout=subprocess.PIPE).communicate()[0]
     return any(('ESSID:"%s"' % ssid) in conn_data for ssid in bad_ssids)
 
 def upgrade_packages(debug=False):
     cmds= """
-        apt-get update
-        apt-get upgrade
-        apt-get autoremove
-        apt-get autoclean
-        """.strip().splitlines()
+        apt-get update -y
+        apt-get upgrade -y
+        apt-get autoremove -y
+        apt-get autoclean -y
+    """.strip().splitlines()
 
     for cmd in cmds:
-        do(cmd + ' -y', debug=True)
+        do(cmd, debug=True)
 
 def main():
-    if on_mifi():
-        print >>sys.stderr, "On personal mobile hotspot so don't update"
+    bad_ssids = ()
+    if sys.argv[1:]:
+        bad_ssids = sys.argv[1:]
+    if on_mifi(bad_ssids):
+        print "On personal mobile hotspot so don't update"
         sys.exit()
     upgrade_packages(debug=True)
 
